25617f694d4ba3b3f5b9ad31469b10ae81d115bb
NandhuPillai <65414672+NandhuPillai@users.noreply.github.com>
2023-08-02 17:37:08 -0400

    all work until end of wednesday

--- dif_modelstruct.py
+++ dif_modelstruct.py
@@ -5,16 +5,37 @@ import tensorflow_hub as hub
 import pathlib
 import deeplake
 import numpy as np
+from sklearn.utils import shuffle
 
+print("Loading dataset...")
 ds = deeplake.load('hub://activeloop/plantvillage-without-augmentation')
 
-img = ds.images[0:1000].numpy()
-label = ds.labels[0:1000].numpy(aslist=True)
-labels = np.array(label)
+print("Dataset loaded...")
+
+# idxs = np.arange(len(ds.labels))
+# np.random.shuffle(idxs)
+
+# small_batch = int(idxs[:100])
+# print(small_batch)
+
+print("Converting to numpy arrays...")
+img = ds.images.numpy()
+labels = ds.labels.numpy()
+
+print("Shuffling data...")
+img, labels = shuffle(img, labels)
+
+img = img[0:100]
+
+labels = labels[0:100]
+# batch_labels = []
+# for i in small_batch:
+#     batch_labels += [label[i]]
+
 labels = labels.flatten()
 labels = tf.convert_to_tensor(labels)
 img = tf.convert_to_tensor(img)
-print("label", label)
+# print("label", label)
 print("labels", labels)
 
 
@@ -25,8 +46,8 @@ model = keras.applications.ResNet152(include_top=False, input_shape=(256, 256, 3
 tuned_model = models.Sequential([
     model,
     layers.GlobalAveragePooling2D(),
-    #layers.Dense(1024, activation='relu'),
-    layers.Dense(38)
+    layers.Dense(1024, activation='relu'),
+    layers.Dense(39)
 ])
 
 for layer in model.layers:
@@ -38,10 +59,11 @@ tuned_model.compile(
     metrics=["accuracy"],
 )
 
-small_train_images = img[0:10]
-small_train_labels = labels[0:10]
+small_train_images = img
+small_train_labels = labels
 
 print(small_train_images.shape)
 print(small_train_labels.shape)
 
-tuned_model.fit(small_train_images, small_train_labels, epochs=10, verbose=2)
+history = tuned_model.fit(small_train_images, small_train_labels, epochs=5, verbose=2)
+
---
+++ load_data.py
@@ -0,0 +1,42 @@
+import numpy as np
+import itertools, pickle
+
+class Database:
+
+    def __init__(self, file = None):
+        
+        if file != None:
+            self.load(file)
+
+
+    def save(self, file):
+        """
+        Saves the database in a .pkl file that can be loaded later.
+
+        Parameters
+        ----------
+        file : str
+            The name of the file to which the database will be saved.
+
+        Returns
+        -------
+        None
+        """
+        with open(file, mode="wb") as opened_file:
+            pickle.dump(self, opened_file)
+
+    def load(self, file):
+        """
+        Loads the database from a .pkl file.
+
+        Parameters
+        ----------
+        file : str
+            The name of the file from which the database will be loaded.
+
+        Returns
+        -------
+        None
+        """
+        with open(file, mode="rb") as opened_file:
+            loaded_db = pickle.load(opened_file)
