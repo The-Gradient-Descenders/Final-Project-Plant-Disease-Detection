e45393e20c497af5a75a94d7627344606fa33303
Nandhu Pillai <65414672+NandhuPillai@users.noreply.github.com>
2023-08-04 10:16:00 -0400

    new way of loading

--- dif_modelstruct.py
+++ dif_modelstruct.py
@@ -1,6 +1,6 @@
 import tensorflow as tf 
 from tensorflow import keras
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, regularizers
 import tensorflow_hub as hub
 import pathlib
 import deeplake
@@ -17,13 +17,18 @@ import math
 start = time.time()
 
 
+train_data_images, train_data_labels, test_data_images, test_data_labels, val_images, val_labels = p.load_data()
 
-train_data_images = np.memmap("./village_train_img.dat", mode="r", shape=(40727, 256, 256, 3))
-test_data_images = np.memmap("./village_test_img.dat", mode="r", shape=(13576, 256, 256, 3))
-train_data_labels = np.memmap("./village_train_labels.dat", mode="r", shape=(40727,))
-test_data_labels = np.memmap("./village_test_labels.dat", mode="r", shape=(13576,))
 
-print("Finished loading data.")
+
+#p.loading_data()
+# train_data_images = np.memmap("./village_train_img.dat", mode="r", shape=(42243, 256, 256, 3))
+# test_data_images = np.memmap("./village_test_img.dat", mode="r", shape=(5280, 256, 256, 3))
+# train_data_labels = np.memmap("./village_train_labels.dat", mode="r", shape=(42243,))
+# test_data_labels = np.memmap("./village_test_labels.dat", mode="r", shape=(5280,))
+# val_images = np.memmap("./village_val_images.dat", mode="r", shape=(5280, 256, 256, 3))
+# val_labels = np.memmap("./village_val_labels.dat", mode="r", shape=(5280,))
+# print("Finished loading data.")
 
 # train_dataset = tf.data.Dataset.from_tensor_slices((train_data_images, train_data_labels))
 # test_dataset = tf.data.Dataset.from_tensor_slices((test_data_images, test_data_labels))
@@ -41,7 +46,7 @@ print("Finished loading data.")
 # print("Shuffled and batched datasets.")
 
 # print("Shuffling data...")
-img, labels = shuffle(train_data_images, train_data_labels)
+#img, labels = shuffle(train_data_images, train_data_labels)
 
 
 
@@ -58,8 +63,12 @@ base_model = keras.applications.ResNet50(include_top=False, input_shape=(256, 25
 #also that additional dense layer could be causing trouble if it's too large
 
 base_model.trainable = False
-# for layer in model.layers:
-#     layer.trainable = False
+# Fine-tune from this layer onwards
+#fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+# for layer in base_model.layers[:fine_tune_at]:
+#   layer.trainable = False
 
 
 
@@ -67,14 +76,14 @@ tuned_model = models.Sequential([
     base_model,
     layers.GlobalAveragePooling2D(),
     layers.Dropout(0.2),
-    # layers.Dense(1024, activation='relu'),
+    layers.Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
     layers.Dense(39)
 ]) # see https://www.tensorflow.org/guide/keras/transfer_learning, this part might be wrong
 
 print(tuned_model.summary())
 
 tuned_model.compile(
-    optimizer=keras.optimizers.Adam(),
+    optimizer=keras.optimizers.Adam(learning_rate=0.0001),
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
@@ -110,13 +119,13 @@ class PlantVillageSequence(tf.keras.utils.Sequence):
 
         return batch_x, batch_y
 
-pvs = PlantVillageSequence(img, labels, 10)
+pvs = PlantVillageSequence(train_data_images, train_data_labels, 64)
 
 
 print("Training model...")
-tuned_model.fit(pvs, epochs=5, verbose=1)
-
+tuned_model.fit(pvs, epochs=1, verbose=1, validation_data=(val_images, val_labels))
 tuned_model.evaluate(test_data_images, test_data_labels)
-
+print("Done training and evaluating.")
+tuned_model.save("savedmodel/")
 end = time.time()
 print("The execution time of the program is ", (end-start), " seconds")
--- processing.py
+++ processing.py
@@ -2,29 +2,65 @@ from load_data import Database
 import numpy as np
 db = Database()
 from collections import Counter
+import deeplake
+from sklearn.utils import shuffle
 
 def loading_data():
     
-    train_images = np.memmap("./village_train_img.dat", dtype="uint8", mode="w+", shape=(40727, 256, 256, 3))
-    train_data_images = db.load("./data/train_images_plantVillage.pkl")
+    train_images = np.memmap("./village_train_img.dat", dtype="uint8", mode="w+", shape=(42243, 256, 256, 3))
+    train_data_images = db.load("./data2/train_images_plantVillage.pkl")
     train_images = train_data_images
     del train_data_images
     
-    test_images = np.memmap("./village_test_img.dat", dtype="uint8", mode="w+", shape=(13576, 256, 256, 3))
-    test_data_images = db.load("./data/test_images_plantVillage.pkl")
+    test_images = np.memmap("./village_test_img.dat", dtype="uint8", mode="w+", shape=(5280, 256, 256, 3))
+    test_data_images = db.load("./data2/test_images_plantVillage.pkl")
     test_images = test_data_images
     del test_data_images
     
-    train_labels = np.memmap("./village_train_labels.dat", dtype="uint8", mode="w+", shape=(40727,))
-    train_data_labels = db.load("./data/train_labels_plantVillage.pkl")
+    val_images = np.memmap("./village_val_images.dat", dtype="uint8", mode="w+", shape=(5280, 256, 256, 3))
+    val_data_images = db.load("./data2/validation_images_plantVillage.pkl")
+    val_images = val_data_images
+    del val_data_images
+
+    train_labels = np.memmap("./village_train_labels.dat", dtype="uint8", mode="w+", shape=(42243,))
+    train_data_labels = db.load("./data2/train_labels_plantVillage.pkl")
     train_labels = train_data_labels
 
-    test_labels = np.memmap("./village_test_labels.dat", dtype="uint8", mode="w+", shape=(13576,))
-    test_data_labels = db.load("./data/test_labels_plantVillage.pkl")
+    test_labels = np.memmap("./village_test_labels.dat", dtype="uint8", mode="w+", shape=(5280,))
+    test_data_labels = db.load("./data2/test_labels_plantVillage.pkl")
     test_labels = test_data_labels
     del test_data_labels
+
+    val_labels = np.memmap("./village_val_labels.dat", dtype="uint8", mode="w+", shape=(5280,))
+    val_data_labels = db.load("./data2/validation_labels_plantVillage.pkl")
+    val_labels = val_data_labels
+    del val_data_labels
+
     # print(Counter(train_data_labels))
     return
+
+def load_data():
+    
+    ds = deeplake.load('hub://activeloop/plantvillage-without-augmentation')
+    imgs = ds.images.numpy()   
+    labels = ds.labels.numpy()
+    labels = labels.flatten()
+    print(imgs.shape)
+    print(labels.shape)
+
+    print("Data Loaded")
+
+    print("Shuffling Data")
+
+    imgs, labels = shuffle(imgs, labels)
+
+    print(imgs.shape)
+    print(labels.shape)
+
+    print("Shuffled Data")
+    
+    return imgs[0:42243], labels[0:42243], imgs[42243:47523], labels[42243:47523], imgs[47523:52803], labels[47523:52803]
+
 
 # def preprocessing():
 #     train_images = np.memmap("./village_train_img.dat", dtype="uint8", mode="w+", shape=(42601, 256, 256, 3))
